84bf372a76ff54a237fb44dc1f512b2a65c1d9c3
Lj <tnedutsl457892@gmail.com>
2021-05-12 14:21:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,7 +6,7 @@
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
-def selects_the_titles_of_all_projects_and_their_pledge_amounts
+def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, SUM(pledges.amount)
   FROM projects
   INNER JOIN pledges
@@ -15,7 +15,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts
   ORDER BY projects.title;"
 end
 
-def selects_the_user_name_age_and_pledge_amount_for_all_pledges
+def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT users.name, users.age, SUM(pledges.amount)
   FROM users
   INNER JOIN pledges
@@ -31,7 +31,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
     GROUP BY projects.title HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
-def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
+def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, SUM(pledges.amount)
   FROM users
   JOIN pledges ON users.id = pledges.user_id
@@ -46,7 +46,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   WHERE projects.category = 'music';"
 end
 
-def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
+def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
   "SELECT projects.category, SUM(pledges.amount)
   FROM projects
   JOIN pledges ON projects.id = pledges.project_id
